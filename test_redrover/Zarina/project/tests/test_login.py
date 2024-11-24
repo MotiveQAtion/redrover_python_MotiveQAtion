da7700ba3fcc068489f56a85a14bafd67aa0748a
Zarina <zaya110589@gmail.com>
2024-11-24 03:25:16 -0500

    j

---
+++ test_redrover/Zarina/project/tests/test_login.py
@@ -0,0 +1,14 @@
+import time
+from test_redrover.Zarina.project.data.urls import *
+from test_redrover.Zarina.project.locators.project_locators import *
+
+
+class TestLogin:
+    def test_login(self, driver):
+        driver.get(BASE_URL)
+        driver.find_element(*LOGIN_BTN).click()
+        driver.find_element(*LOGIN_USERNAME).send_keys('ZarUmb')
+        driver.find_element(*LOGIN_PASSWORD).send_keys('Miras110589')
+        driver.find_element(*SUBMIT_LOGIN_BUTTON).click()
+        time.sleep(5)
+        assert driver.current_url == LOGIN_URL
