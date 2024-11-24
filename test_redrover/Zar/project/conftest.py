3eb825d47dea6d86c6289a12f3e2ed3bfd0180c5
Zarina <zaya110589@gmail.com>
2024-11-24 02:35:11 -0500

    Revert "testreg/testlogin"
    This reverts commit 6f1173219083eece09c8b01e96afe314ef0aa38d.

--- test_redrover/Zar/project/conftest.py
+++ test_redrover/Zar/project/conftest.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 @pytest.fixture
-def Zaya():
+def driver():
     service = Service(executable_path=ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
     # options.add_argument("--window-size=1900,1000")
